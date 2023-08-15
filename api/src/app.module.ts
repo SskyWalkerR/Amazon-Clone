5c16d61ebf37bfd3206ca8b394032615a7acca8d
SskyWalkerR <sammanualvarghese@gmail.com>
2023-08-15 20:42:12 +0530

    api/basic - fix: secret

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,14 +1,17 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductModule } from './product/product.module';
 
 @Module({
-  imports: [MongooseModule.forRoot(''), ProductModule],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGODB_URL),
+    ProductModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
-
-// mongodb+srv://clone_threads:VSiTAwNejF4i6QoG@cluster0.xayhqmc.mongodb.net/?retryWrites=true&w=majority
