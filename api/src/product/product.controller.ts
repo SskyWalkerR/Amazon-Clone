9d31b21a34821f78b86dd3ab6838d5892340bed0
SskyWalkerR <sammanualvarghese@gmail.com>
2023-08-19 19:49:04 +0530

    api/basic - feat: Product CRUD

--- api/src/product/product.controller.ts
+++ api/src/product/product.controller.ts
@@ -1,11 +1,49 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  Get,
+  Param,
+  Patch,
+  Delete,
+} from '@nestjs/common';
+import { ProductService } from './product.service';
 
 @Controller('product')
 export class ProductController {
+  constructor(private productService: ProductService) {}
+
   @Post()
   createProduct(
     @Body('name') name: string,
-    @Body('price') price: string,
+    @Body('price') price: number,
     @Body('description') description?: string,
-  ) {}
+  ) {
+    return this.productService.createProduct(name, price, description);
+  }
+
+  @Get()
+  findAllProducts() {
+    return this.productService.findAllProducts();
+  }
+
+  @Get(':id')
+  findProduct(@Param('id') id: string) {
+    return this.productService.findProduct(id);
+  }
+
+  @Patch(':id')
+  updateProduct(
+    @Param('id') id: string,
+    @Body('name') name: string,
+    @Body('price') price: number,
+    @Body('description') description?: string,
+  ) {
+    return this.productService.updateProduct(id, name, price, description);
+  }
+
+  @Delete(':id')
+  deleteProduct(@Param('id') id: string) {
+    return this.productService.deleteProduct(id);
+  }
 }
--- api/src/product/product.service.ts
+++ api/src/product/product.service.ts
@@ -1,4 +1,48 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Product, ProductDocument } from './product.schema';
 
 @Injectable()
-export class ProductService {}
+export class ProductService {
+  constructor(
+    @InjectModel(Product.name)
+    private readonly productModel: Model<ProductDocument>,
+  ) {}
+
+  async createProduct(
+    name: string,
+    price: number,
+    description?: string,
+  ): Promise<ProductDocument> {
+    const newProduct = new this.productModel({ name, price, description });
+    return newProduct.save();
+  }
+
+  async findAllProducts(): Promise<ProductDocument[]> {
+    return this.productModel.find().exec();
+  }
+
+  async findProduct(id: string): Promise<ProductDocument> {
+    return this.productModel.findById(id).exec();
+  }
+
+  async updateProduct(
+    id: string,
+    newName: string,
+    newPrice: number,
+    newDescription?: string,
+  ): Promise<ProductDocument> {
+    const existingProduct = await this.findProduct(id);
+
+    existingProduct.name = newName ?? existingProduct.name;
+    existingProduct.price = newPrice ?? existingProduct.price;
+    existingProduct.description = newDescription ?? existingProduct.description;
+
+    return existingProduct.save();
+  }
+
+  async deleteProduct(id: string) {
+    return this.productModel.deleteOne({ _id: id }).exec();
+  }
+}
