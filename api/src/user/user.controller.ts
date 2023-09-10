9c2599c116f63a23099ba1028366d4aedea63e73
SskyWalkerR <sammanualvarghese@gmail.com>
2023-09-10 09:24:27 +0530

    feat/auth - fix: Auth guard user obj

--- api/src/user/user.controller.ts
+++ api/src/user/user.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Request } from '@nestjs/common';
+import { UserDetails } from './types';
+import { UserService } from './user.service';
 
-@Controller('user')
-export class UserController {}
+@Controller('users')
+export class UserController {
+  constructor(private userService: UserService) {}
+
+  @Get('me')
+  async getUser(@Request() req: any): Promise<UserDetails | null> {
+    const userId = req.userData.user.id;
+    const user = await this.userService.findUserById(userId);
+    return user;
+  }
+}
